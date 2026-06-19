bcc7055916fb23fa44d079f34de293d30b448f30
Andres Bravo <andbrav1917@gmail.com>
2026-06-19 06:28:38 -0400

    (feat): read input command history 1.2 completed

--- cmd/root.go
+++ cmd/root.go
@@ -1,51 +1,70 @@
-/*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
+var inputDir string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "media-converter",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Convert images and videos",
+	Long: `Media Converter is a CLI tool built with Go
+to process files concurrently using worker pools.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+
+		info, err := os.Stat(inputDir)
+
+		if err != nil {
+			fmt.Printf(
+				"Input directory '%s' does not exist\n",
+				inputDir,
+			)
+			return
+		}
+
+		if !info.IsDir() {
+			fmt.Printf(
+				"'%s' is not a directory\n",
+				inputDir,
+			)
+			return
+		}
+
+		fmt.Printf(
+			"Input directory found: %s\n",
+			inputDir,
+		)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
+
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.media-converter.yaml)")
+	rootCmd.Flags().StringVarP(
+		&inputDir,
+		"input",
+		"i",
+		"",
+		"Input directory",
+	)
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	err := rootCmd.MarkFlagRequired("input")
+
+	if err != nil {
+		panic(err)
+	}
 }
-
-
